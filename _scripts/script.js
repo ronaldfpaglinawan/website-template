6fe94071025134c520d441ddae758f37ae5e067f
Ronald Paglinawan <ronaldpaglinawan@Ronalds-MacBook-Pro.local>
2018-08-18 18:07:24 +1200

    added files for the website

---
+++ _scripts/script.js
@@ -0,0 +1,19 @@
+// global variables
+var firstString = "Welcome to";
+var secondString = "My Basic Web Page Template";
+
+function changeMainTitle() {
+//    var h1Element = document.getElementById("mainTitle");
+//    var message = firstString + secondsRemaining;
+//    
+//    h1Element.innerHTML = message;
+//    
+//    document.getElementById("mainTitle").appendChild(h1Element);
+    
+//    alert("Hello Javascript!");
+}
+
+window.onload = function() {
+    changeMainTitle();
+};
+
